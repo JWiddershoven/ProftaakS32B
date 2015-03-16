6fee49b521ea99e2090094c774a2623144080f37
spmvanmierlo <spm.vanmierlo@student.fontys.nl>
2015-03-16 14:09:10 +0100

    Server class

--- BreakingPong/src/Server/Server.java
+++ BreakingPong/src/Server/Server.java
@@ -6,10 +6,50 @@
 
 package Server;
 
+import Shared.User;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
+ * The server class
  * @author Mnesymne
  */
 public class Server {
+    
+    /*
+    List with all the current users
+    */
+    private List<User> users;
+    
+    /*
+    List with all the current lobby's
+    */
+    private List<Lobby> lobbys;
+    
+    /**
+     * Constructor
+     */
+    public Server(){
+        users = new ArrayList<>();
+        lobbys = new ArrayList<>();
+    }
+    
+    /**
+     * Checks if the selected lobby exists 
+     * and the lobby is not full 
+     * and checks if there is a password 
+     * and checks if the password is correct
+     * @param lobby The lobby the user wants to join.
+     */
+    public void JoinLobby(Lobby lobby){
+        
+    }
+    
+    /**
+     * Logs the current user off 
+     */
+    public void LogOut(){
+        
+    }
     
 }
