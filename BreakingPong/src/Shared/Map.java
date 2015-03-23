fccfd1befa45790aae2116363578b4c8bb1fd317
Jelle Widdershoven <jellewiddershoven@gmail.com>
2015-03-23 19:47:14 +0100

    Map class gespecificeerd

--- BreakingPong/src/Shared/Map.java
+++ BreakingPong/src/Shared/Map.java
@@ -11,4 +11,55 @@ package Shared;
  */
 public class Map {
     
+    private String name;
+    private int playerAmount;
+    private int[][] layout;
+    
+    /**
+     * Creates a new Map object with a name, playerAmount and layout.
+     * @param name The name of the map. Name cannot be an empty string.
+     * @param playerAmount The amount of players of the map. playerAmount has to be 2 or 4.
+     * @param layout The layout of the map.
+     */
+    public Map(String name, int playerAmount, int[][] layout)
+    {
+        this.name = name;
+        this.playerAmount = playerAmount;
+        this.layout = layout;
+    }
+    
+    /**
+     * @return The name of the map.
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    /**
+     * @return The amount of players of the map.
+     */
+    public int getPlayerAmount()
+    {
+        return this.getPlayerAmount();
+    }
+    /**
+     * Delete the map object.
+     */
+    public void deleteMap()
+    {
+        
+    }
+    /**
+     * Updates the map object.
+     * @param name The name of the map.
+     * @param playerAmount The amount of players of the map.
+     * @param layout The layout of the map.
+     */
+    public void updateMap(String name, int playerAmount, int[][] layout)
+    {
+        this.name = name;
+        this.playerAmount = playerAmount;
+        this.layout = layout;
+    }
 }
