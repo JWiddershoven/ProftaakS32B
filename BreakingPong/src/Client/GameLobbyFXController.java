9160a771c99c8e186e491ce978eedf1ed4daa2ec
Jordi Jacobs <jordo538@hotmail.com>
2015-03-31 22:24:09 +0200

    InGame GUI Added, startgame button fixed

--- BreakingPong/src/Client/GameLobbyFXController.java
+++ BreakingPong/src/Client/GameLobbyFXController.java
@@ -5,13 +5,22 @@
  */
 package Client;
 
+import static Client.ClientGUI.MainStage;
 import Server.Administration;
+import Server.Lobby;
+import java.awt.HeadlessException;
+import java.awt.TrayIcon;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -43,7 +52,14 @@ public class GameLobbyFXController implements Initializable {
     
     @FXML
     private void onStartGameClick(){
-        System.out.println("game starte");
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("InGame.fxml"));
+            Scene scene = new Scene(root);
+            MainStage.setScene(scene);
+            MainStage.show();
+        } catch (IOException | HeadlessException ex) {
+
+        }
     }
     
 }
---
+++ BreakingPong/src/Client/InGameFXController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import static Client.ClientGUI.MainStage;
+import Shared.Game;
+import java.awt.Color;
+import javafx.application.Application;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+/**
+ *
+ * @author Jordi
+ */
+public class InGameFXController implements Initializable {
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) 
+    {
+
+    }
+}
