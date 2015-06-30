27b6877b35e0cba81ac56134e5f56d33ffbba13a
Jordi Jacobs <jordo538@hotmail.com>
2015-06-30 15:48:01 +0200

    sout client lobby id

--- BreakingPong/src/RMI/Client.java
+++ BreakingPong/src/RMI/Client.java
@@ -149,6 +149,7 @@ public class Client extends Application implements RemotePropertyListener {
                     try
                     {
                         System.out.println("left");
+                        System.out.println(this.gui.joinedLobby.getLobbyID());
                         connection.moveLeft(this.gui.joinedLobby.getLobbyID(), this.Name);
                         
                     } catch (RemoteException ex)
@@ -163,6 +164,7 @@ public class Client extends Application implements RemotePropertyListener {
                     try
                     {
                         System.out.println("right");
+                        System.out.println(this.gui.joinedLobby.getLobbyID());
                         connection.moveRight(this.gui.joinedLobby.getLobbyID(), this.Name);
                     }
                     catch (RemoteException ex) {
